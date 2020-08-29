f84e04c9149623b6634c57c55c477b8afbd151e6
nikfakhri06 <41029854+nikfakhri06@users.noreply.github.com>
2020-08-29 09:47:48 +0800

    Add click function

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
+
+rock_div.addEventListener("click", function () {
+  console.log("you clicked on rock");
+});
+
+paper_div.addEventListener("click", function () {
+  console.log("you clicked on paper");
+});
+
+scissor_div.addEventListener("click", function () {
+  console.log("you clicked on scissor");
+});
