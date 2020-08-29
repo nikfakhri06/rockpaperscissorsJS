5ebdf66bbcd69bd95f87f31a502e9cf1c4d8f9ee
nikfakhri06 <41029854+nikfakhri06@users.noreply.github.com>
2020-08-29 11:39:45 +0800

    finshed JS. added glow ring. ned to check why image not load

--- app.js
+++ app.js
@@ -1,21 +1,100 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
 
-rock_div.addEventListener("click", function () {
-  console.log("you clicked on rock");
-});
+function getComputerChoice() {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
 
-paper_div.addEventListener("click", function () {
-  console.log("you clicked on paper");
-});
+function convertToWord(letter) {
+  if (letter === "r") return "✊ Rock";
+  if (letter === "p") return "✋Paper";
+  return "✌ Scissor";
+}
 
-scissor_div.addEventListener("click", function () {
-  console.log("you clicked on scissor");
-});
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "(user)".fontsize(3);
+  const smallCompWord = "(comp)".fontsize(3);
+  result_p.innerHTML = `${convertToWord(
+    userChoice
+  )}${smallUserWord} beats ${convertToWord(
+    computerChoice
+  )}${smallCompWord}. You Win! 🔥`;
+  document.getElementById(userChoice).classList.add("green-glow");
+  setTimeout(function () {
+    document.getElementById(userChoice).classList.remove("green-glow");
+  });
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "(user)".fontsize(3);
+  const smallCompWord = "(comp)".fontsize(3);
+  result_p.innerHTML = `${convertToWord(
+    userChoice
+  )}${smallUserWord} loses to ${convertToWord(
+    computerChoice
+  )}${smallCompWord}. You lost...💩`;
+}
+
+function draw(userChoice, computerChoice) {
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "(user)".fontsize(3);
+  const smallCompWord = "(comp)".fontsize(3);
+  result_p.innerHTML = `${convertToWord(
+    userChoice
+  )}${smallUserWord} equals ${convertToWord(
+    computerChoice
+  )}${smallCompWord}. It's a draw`;
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
+function main() {
+  rock_div.addEventListener("click", function () {
+    game("r");
+  });
+
+  paper_div.addEventListener("click", function () {
+    game("p");
+  });
+
+  scissor_div.addEventListener("click", function () {
+    game("s");
+  });
+}
+
+main();
